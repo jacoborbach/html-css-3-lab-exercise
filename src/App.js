b46da880091a10afaa502c0070fd390e214eab59
jacoborbach <jacoborbach96@gmail.com>
2021-01-07 16:47:22 -0500

    latest

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {/* Navbar */}
+      <div className='navbar'>
+        <h3 className='logo'>Start Boostrap</h3>
+        <nav className='nav'>
+          <a href='google.com'>SERVICES</a>
+          <a href='google.com'>PORTFOLIO</a>
+          <a href='google.com'>ABOUT</a>
+          <a href='google.com'>TEAM</a>
+          <a href='google.com'>CONTACT</a>
+        </nav>
+      </div>
+
+
+
+
+      {/* Header aka picture with header */}
       <header className="header">
         <div className='container'>
           <div className='subheading'>
